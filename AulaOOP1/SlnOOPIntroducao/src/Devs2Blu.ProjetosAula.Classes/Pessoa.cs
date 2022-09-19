249300ba82d3e2bd2bbc94d1e17ef84dd7ede460
Eduardo A Cestari <eduardo.cestari@gmail.com>
2022-09-19 18:55:43 -0300

    Atualizando Repositório

---
+++ AulaOOP1/SlnOOPIntroducao/src/Devs2Blu.ProjetosAula.Classes/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devs2Blu.ProjetosAula.Classes
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public int Idade { get; set; }
+        public DataSetDateTime DataNascimento { get; set; }
+        public string Endereco { get; set; }
+
+        public string ApresentarNomeCustom()
+        {
+            string nomeCustom;
+
+            nomeCustom = $"{SobreNome}, {Nome}";
+
+            return nomeCustom;
+        }
+    }
+}
---
+++ Solution_8/WindowsForms_2/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace WindowsForms_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbAdress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbBorno = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSname = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.rbRecepcionista = new System.Windows.Forms.RadioButton();
+            this.rbDiretor = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbViewFuncs = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnErase = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.groupBox1.Controls.Add(this.tbAdress);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbBorno);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbSname);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Location = new System.Drawing.Point(16, 25);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(356, 331);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro de Funcionários";
+            // 
+            // tbAdress
+            // 
+            this.tbAdress.Location = new System.Drawing.Point(95, 93);
+            this.tbAdress.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbAdress.Name = "tbAdress";
+            this.tbAdress.Size = new System.Drawing.Size(125, 20);
+            this.tbAdress.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 96);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Endereço";
+            // 
+            // tbBorno
+            // 
+            this.tbBorno.Location = new System.Drawing.Point(95, 68);
+            this.tbBorno.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbBorno.Name = "tbBorno";
+            this.tbBorno.Size = new System.Drawing.Size(125, 20);
+            this.tbBorno.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nascimento";
+            // 
+            // tbSname
+            // 
+            this.tbSname.Location = new System.Drawing.Point(228, 33);
+            this.tbSname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbSname.Name = "tbSname";
+            this.tbSname.Size = new System.Drawing.Size(125, 20);
+            this.tbSname.TabIndex = 3;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(95, 33);
+            this.tbName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(125, 20);
+            this.tbName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.rbRecepcionista);
+            this.groupBox4.Controls.Add(this.rbDiretor);
+            this.groupBox4.Location = new System.Drawing.Point(49, 152);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox4.Size = new System.Drawing.Size(201, 96);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Tipo Funcionário";
+            // 
+            // rbRecepcionista
+            // 
+            this.rbRecepcionista.AutoSize = true;
+            this.rbRecepcionista.Location = new System.Drawing.Point(110, 43);
+            this.rbRecepcionista.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbRecepcionista.Name = "rbRecepcionista";
+            this.rbRecepcionista.Size = new System.Drawing.Size(93, 17);
+            this.rbRecepcionista.TabIndex = 3;
+            this.rbRecepcionista.Text = "Recepcionista";
+            this.rbRecepcionista.UseVisualStyleBackColor = true;
+            // 
+            // rbDiretor
+            // 
+            this.rbDiretor.AutoSize = true;
+            this.rbDiretor.Checked = true;
+            this.rbDiretor.Location = new System.Drawing.Point(22, 43);
+            this.rbDiretor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbDiretor.Name = "rbDiretor";
+            this.rbDiretor.Size = new System.Drawing.Size(56, 17);
+            this.rbDiretor.TabIndex = 2;
+            this.rbDiretor.TabStop = true;
+            this.rbDiretor.Text = "Diretor";
+            this.rbDiretor.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Gainsboro;
+            this.groupBox2.Controls.Add(this.tbViewFuncs);
+            this.groupBox2.Location = new System.Drawing.Point(386, 25);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(190, 200);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
+            // tbViewFuncs
+            // 
+            this.tbViewFuncs.BackColor = System.Drawing.Color.Gainsboro;
+            this.tbViewFuncs.Location = new System.Drawing.Point(14, 17);
+            this.tbViewFuncs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbViewFuncs.Multiline = true;
+            this.tbViewFuncs.Name = "tbViewFuncs";
+            this.tbViewFuncs.Size = new System.Drawing.Size(164, 170);
+            this.tbViewFuncs.TabIndex = 0;
+            this.tbViewFuncs.Text = "Cadastro de funcionáros";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Gainsboro;
+            this.groupBox3.Controls.Add(this.btnErase);
+            this.groupBox3.Controls.Add(this.btnCancel);
+            this.groupBox3.Controls.Add(this.btnSave);
+            this.groupBox3.Location = new System.Drawing.Point(386, 230);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Size = new System.Drawing.Size(190, 126);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Controles";
+            // 
+            // btnErase
+            // 
+            this.btnErase.BackColor = System.Drawing.Color.Red;
+            this.btnErase.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnErase.ForeColor = System.Drawing.Color.Black;
+            this.btnErase.Location = new System.Drawing.Point(24, 82);
+            this.btnErase.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnErase.Name = "btnErase";
+            this.btnErase.Size = new System.Drawing.Size(142, 24);
+            this.btnErase.TabIndex = 2;
+            this.btnErase.Text = "Apagar";
+            this.btnErase.UseVisualStyleBackColor = false;
+            this.btnErase.Click += new System.EventHandler(this.btnErase_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Gold;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.Black;
+            this.btnCancel.Location = new System.Drawing.Point(24, 54);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(142, 24);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Limpar";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.Black;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSave.Location = new System.Drawing.Point(24, 24);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(142, 24);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Salvar";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSave.UseMnemonic = false;
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Cadastro de funcionário";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbAdress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbBorno;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbSname;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbViewFuncs;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RadioButton rbRecepcionista;
+        private System.Windows.Forms.RadioButton rbDiretor;
+        private System.Windows.Forms.Button btnErase;
+    }
+}
+
