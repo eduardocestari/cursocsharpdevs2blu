af4db0710d4ae37ca24c61d1c144d847b4c8f4ae
Eduardo A Cestari <eduardo.cestari@gmail.com>
2022-11-24 23:26:55 -0300

    Primeiro Projeto MVC

---
+++ CSharpMVC/SlnPrimeiroProjetoMVC/Devs2Blu.ProjetosAula.PrimeiroProjetoMVC/Controllers/GaleriaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Devs2Blu.ProjetosAula.PrimeiroProjetoMVC.Controllers
+{
+    public class GaleriaController : Controller
+    {
+        [Route("galeria")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        public IActionResult Galeria()
+        {
+            return View();
+        }
+
+        public IActionResult Carousel()
+        {
+            return View();
+        }
+    }
+}
---
+++ CSharpMVC/SlnPrimeiroProjetoMVC/Devs2Blu.ProjetosAula.PrimeiroProjetoMVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Devs2Blu.ProjetosAula.PrimeiroProjetoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Devs2Blu.ProjetosAula.PrimeiroProjetoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("home/cadastro")]
+        [Route("cadastro")]
+        [Route("contato")]
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
